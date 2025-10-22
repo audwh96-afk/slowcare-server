ef934ded0aad18925972059396879578f7f5d33c
오토페이 <fbaudwh@naver.com>
2025-10-22 10:50:35 +0900

    Apply changes without secrets

--- src/main/java/com/slow/care/config/FirebaseConfig.java
+++ src/main/java/com/slow/care/config/FirebaseConfig.java
@@ -6,20 +6,21 @@ import com.google.firebase.FirebaseOptions;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
-import javax.annotation.PostConstruct;
 import java.io.IOException;
 import java.io.InputStream;
+
+import javax.annotation.PostConstruct;
 
 @Configuration
 public class FirebaseConfig {
 
     @PostConstruct
     public void initializeFirebase() {
-        try { 
-            // resources 폴더에서 파일 읽기입니다.
-            ClassPathResource resource = new ClassPathResource("slowcare-d48ff-firebase-adminsdk-fbsvc-89aef95574.json");
+        try {
+            // resources 폴더에서 파일 읽기
+            ClassPathResource resource = new ClassPathResource("slowcare-d48ff-firebase-adminsdk-fbsvc-a51daeb1cf.json");
             InputStream serviceAccount = resource.getInputStream();
-            
+
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .build();
